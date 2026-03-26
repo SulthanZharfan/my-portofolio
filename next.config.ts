7793814869545c449ce50f93fe3f587d34d1364a
Muhammad Sulthan Zharfan <zharfan2231@gmail.com>
2026-03-26 13:40:47 +0700

    Update Code

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'my-portofolio';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Ensure the app works correctly at sulthanzharfan.github.io/my-portofolio/
+  basePath: isProd ? `/${repoName}` : undefined,
+  assetPrefix: isProd ? `/${repoName}/` : undefined,
 };
 
 export default nextConfig;
